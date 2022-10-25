05ffb8aecdec04efdadf7a00e0d95c148828c8aa
aviraldewan <aviraldewan@gmail.com>
2022-10-25 21:47:09 +0530

    Updated gitignore file

--- Downloads/OTP_Platform/config.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = {
-    serviceID: "VA7457b25c93df35724f755b6b9f52fb36",
-    accountSID: "AC6e1169d373a22a19aab99d9b580a0168",
-    authToken: "6304be278323cb25a45925f625a2f8ad",
-}
